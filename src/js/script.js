43ed29524a8e283cbd54e18e3fa06c95c5203142
Oleg Grigorev <osrolex@yandex.ru>
2022-07-29 16:19:34 +0500

    minor overall changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,12 @@
 const burger = document.querySelector('.burger');
 const menuItems = document.querySelectorAll('.menu__item');
+const violetBgTop = document.querySelector('.violet-bg-top');
+const introDescr = document.querySelector('.intro__description');
 
 burger.addEventListener('click', () => {
+	// violetBgTop.classList.toggle('violet-bg-top--enlarge')
+	// introDescr.style.marginTop = '0';
 	menuItems.forEach(item => {
-		item.classList.toggle('active');
+		item.classList.toggle('menu__item--active');
 	})
 });
